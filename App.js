614332a2b975b4de94e5589e8a7f2b631176c67c
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2018-08-01 15:29:20 +0100

    add textfield to demo page

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Formio from 'formiojs/Formio'
+import {StyleSheet, View, Text} from 'react-native';
+import {Formio} from 'react-native-formio/src';
 
-const form = new Formio('https://examples.form.io/textfield');
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      formData: ''
-    }
-  }
-
-  componentDidMount() {
-    form.loadForm().then((form) => {
-      console.log(form);
-      this.setState({ formData: JSON.stringify(form) })
-    });
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>React Native Formio Demo</Text>
-        <Text>{this.state.formData}</Text>
+        <Text style={styles.text}>Formio Demo</Text>
+        <Formio src={'https://examples.form.io/example'}/>
       </View>
     );
   }
@@ -30,18 +16,14 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    flexDirection: 'column',
   },
-  welcome: {
+  text: {
+    marginTop: 30,
+    alignSelf: 'center',
+    fontWeight: 'bold',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    marginBottom: 20,
   },
 });
